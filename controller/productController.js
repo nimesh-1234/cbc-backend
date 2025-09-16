0c2b417eb5fadc59bce6f4c0498bfdebec4e53a7
Nimesh Dilshan <nimeshdilshan869@gmail.com>
2025-09-16 10:36:22 +0530

    day 19✏️

--- controller/productController.js
+++ controller/productController.js
@@ -1,148 +1,125 @@
 import Product from "../models/product.js";
-import productRouter from "../routes/productRouter.js";
 import { isAdmin } from "./userController.js";
 
-export  async function createProduct(req,res){
+export async function createProduct(req, res) {
 
-if (!isAdmin(req)) {
-    
-     res.status(403).json({
-            message :"your are not authorized to create  a product"
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message: "You are not authorized to create a product"
         });
         return;
-}
-
-    try {
-        
-     const productData = req.body;
-
-     const product = new Product(productData)
-
-     await product.save()
-
-     res.json(
-        {
-            message : "product create successfull",
-            product : product
-        }
-     );
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message : "Faild to create product"
-        })
     }
-    
+
+	try {
+        
+		const productData = req.body;
+
+		const product = new Product(productData);
+
+		await product.save();
+
+		res.json({
+			message: "Product created successfully",
+			product: product,
+		});
+        
+	} catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Failed to create product",
+        });
+	}
 }
 
-export async function getProducts(req, res) {
-    console.log("fetching all product")
-    try {
-
-    const products = await Product.find();
-    res.json(products);
-        
-    } catch (error) {
-        console.error(error);
+export async function getProducts(req,res){
+    console.log("Fetching all products");
+    try {        
+        const products = await Product.find()
+        res.json(products);
+    } catch (err) {
+        console.error(err);
         res.status(500).json({
-
-            message : "Faild  to retrieve products",
+            message: "Failed to retrieve products",
         });
     }
-
 }
 
-
-export async function deleteProduct(req, res) {
-    if (!isAdmin(req)) {
+export async function deleteProduct(req,res){
+    if(!isAdmin(req)){
         res.status(403).json({
             message: "You are not authorized to delete a product"
         });
         return;
     }
+    try{
 
-    try {
-        const productId = req.params.productId; 
+        const productID = req.params.productID
         
 
-
-        // Find the product first
-        const product = await Product.findOne({ productId });
-        
-
-        if (!product) {
-            res.status(404).json({
-                message: "Product not found"
-            });
-            return;
-        }
-
-        // Then delete it
-        await Product.deleteOne({ productId });
+        await Product.deleteOne({
+            productID : productID
+        })
 
         res.json({
             message: "Product deleted successfully"
         });
-
-    } catch (error) {
-        console.error(error);
+    }catch(err){
+        console.error(err);
         res.status(500).json({
-            message: "Failed to delete product"
+            message: "Failed to delete product",
         });
     }
 }
 
-
-export async function updateProduct(req,res) {
-    
-    if (!isAdmin(req)) {
+export async function updateProduct(req,res){
+    if(!isAdmin(req)){
         res.status(403).json({
-            message: "You are not authorized to delete a product"
+            message: "You are not authorized to update a product"
         });
         return;
     }
 
-    try {
-        
-        const productId = req.params.productId; 
-        const updateData = req.body;
-        await Product.findOneAndUpdate(
-            {productId : productId},
-            updateData,
-            { new: true, runValidators: true }
-        );
-        res.json({
-            message : "update succesfully product"
-        })
+    try{
+        const productID = req.params.productID;
 
-    } 
-    catch (error) {
-        console.error(error);
+        const updatedData = req.body;
+
+        await Product.updateOne(
+            {productID : productID},
+            updatedData
+        );
+
+        res.json({
+            message: "Product updated successfully"
+        });
+    }catch(err){
+        console.error(err);
         res.status(500).json({
-            message: "Failed to update product"
+            message: "Failed to update product",
         });
     }
-
 }
 
-export async function getProductId(req, res) {
-  try {
-    const productId = req.params.productId;
-    const product = await Product.findOne({
-      productId: productId,
-    });
+export async function getProductId(req,res){
+    try{
+        const productID = req.params.productID;
 
-    if (product == null) {
-      res.status(404).json({
-        message: "product not found",
-      });
+        const product = await Product.findOne(
+            {
+                productID : productID
+            }
+        )
+        if(product == null){
+            res.status(404).json({
+                message: "Product not found"
+            });
+        }else{
+            res.json(product);
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message: "Failed to retrieve product by ID",
+        });
     }
-    return res.status(200).json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Faild  to retrieve products",
-    });
-  }
 }
--- index.js
+++ index.js
@@ -1,96 +1,67 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import productRouter from "./routes/productRouter.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config();
 
 const app = express()
+app.use(cors())
 
-app.use(cors());
 app.use(express.json())
 
-//athentication
 app.use(
     (req,res,next)=>{
-       let token = req.headers["authorization"];
 
-      if (token != null) {
+        let token = req.header("Authorization")
+
+        if(token != null){
             token = token.replace("Bearer ","")
-            console.log(token)
-
-            //decript
-            jwt.verify(token,process.env.JWT_SECRET, 
-                (err,decoded)=>{
-
-                    if (decoded == null) {
-                        res.json(
-                            {
-                                message : "invalid token please login again"
-                            }
-                        )
+            jwt.verify(token, process.env.JWT_SECRET,
+                (err, decoded)=>{
+                    if(decoded == null){
+                        res.json({
+                            message: "Invalid token please login again"
+                        })
                         return
                     }else{
-                        //console.log(decoded)
                         req.user = decoded
                     }
-            })
-      }
-      next()
+                }
+            )
+
+        }
+        next()
     }
 )
 
-//database 
 const connectionString = process.env.MONGO_URI
 
 
 mongoose.connect(connectionString).then(
     ()=>{
-        console.log("database connected sucessfully")
+        console.log("Database connected Successfully")
     }
 ).catch(
     ()=>{
-        console.log("database not connected")
+        console.log("Database connection failed")
     }
 )
 
-//day5 plug routers
+
+
 
 app.use("/api/users",userRouter)
 app.use("/api/products", productRouter)
+app.use("/api/orders", orderRouter)
 
 
-
-//requeset,response
-/*app.get("/", 
-    (req,res)=>
-{
-    console.log(req.body)
-    console.log("get request recieved")
-
-    let prifix = "Mr."
-    if(req.body.gender == "male"){
-        prifix = "Ms."
-    }
-
-    res.json(
-            {
-                message : "hello "+prifix+" "+req.body.name
-            }
-        )
-}
-)*/
-
-
-
-
-//one time use function
 app.listen(5000, 
     ()=>{
-        console.log("server is running is port 5000")
-        
+        console.log("Server is running on port 5000")
     }
 )
